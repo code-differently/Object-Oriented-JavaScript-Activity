5488252a071f9d2fa60e2473585911d3ca1cb517
Cinitae <74632407+Cinitae@users.noreply.github.com>
2021-01-21 18:49:37 -0500

    Fb post and tweet

---
+++ script.js
@@ -0,0 +1,57 @@
+
+let post = {
+    name: "Team Ever Ready",
+    username: "@eReady",
+    message: "@coderhino why is your code hot garbage?",
+    like: 0,
+    comments: [],
+    location: "Delaware",
+    addLikes: function () {
+        post.like ++;
+    },
+
+    addComment: function (comment) {
+        (post.comments).push(comment);
+    }
+}
+
+post.message = "@coderhino writes code like it's a work of fine art"
+post.time = Math.time;
+post["name"] = "Team EveReady";
+console.log(post.like);
+post.addLikes()
+console.log(post.like);
+
+class Tweet {
+    constructor(name){
+        this.message= [];
+    }
+    addLetter(letter){
+        this.message.push(letter);
+    }
+    removeLetter(){
+        this.message.pop();
+    }
+    viewMessage(){
+        console.log(this.messge);
+    }
+}
+
+post.message = "@coderhino writes code like it's a work of fine art"
+post.time = Math.time;
+post["name"] = "Team EveReady";
+console.log(post.like);
+post.addLikes()
+console.log(post.like);
+
+
+// // properties
+// name 
+// message
+// likes 
+// time 
+// location 
+// tagperson 
+// picture
+// add Comment
+// like increments likes by 1 (++)
