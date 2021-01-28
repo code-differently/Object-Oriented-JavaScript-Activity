3acc3aaa2f312e7a5c20f5c871dbd6b1772a0b34
Talyr Williams <talyrswilliams@gmail.com>
2021-01-27 20:55:37 -0500

    done

---
+++ cars.js
@@ -0,0 +1,12 @@
+class Cars {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+}
+
+let myCar = new Cars();
+myCar(Honda, Civic);
+
+let myCar2 = new Cars();
+myCar2(Fod, Escort);
---
+++ main.js
@@ -0,0 +1,29 @@
+let post1 = {
+    displayName: "Kaleb Burd",
+    picture: "",
+    location: "",
+    comment: "@coderhino why is your code hot garbage?",
+    emoji: ""
+}
+
+let post2 = {
+    displayName: "Kaleb Burd",
+    picture: "",
+    location: "",
+    comment: "@coderhino why is your code hot garbage?",
+    emoji: ""
+}
+
+let post3 = {
+    displayName: "Kaleb Burd",
+    picture: "",
+    location: "",
+    comment: "@coderhino why is your code hot garbage?",
+    emoji: ""
+}
+
+
+console.log(post1.displayName);
+console.log(post1[displayName]);
+
+post3.time = "3 hours";
