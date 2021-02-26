4a4a2fc1496624001aa7c240206223afd07f3020
kcee917 <kcee917@gmail.com>
2021-02-25 19:52:33 -0500

    Completed

---
+++ object.js
@@ -0,0 +1,17 @@
+ var post = newPost();
+    newPost.name = "Kym";
+    newPost.message = "Learning to code is learning a new language!";
+    newPost.numWords = function(numWords) {
+        if (message.length < 141) {
+            return post;
+        } else {
+            return("More than 140 characters!")
+        }
+    } 
+
+    class post {
+        constructor(name, message) {
+          this.name = name;
+          this.message = "View message", "Add letter", "Remove letter";
+        }
+      }
